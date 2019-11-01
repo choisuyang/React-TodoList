181c19c20a8929f9ea7f4d927a746a17dfa77349
choisuyang <chltndid724@gmail.com>
2019-11-01 20:28:29 +0900

    Set-Up

---
+++ todolist/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplateBlock from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplateBlock>안녕하세요</TodoTemplateBlock>
+    </>
+  );
+}
+
+export default App;
